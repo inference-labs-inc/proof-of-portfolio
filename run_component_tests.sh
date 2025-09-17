f9cde4c24f5378cf37d634875e063d6540483cf0
HudsonGraeme <spencer@inferencelabs.com>
2025-09-17 16:27:07 -0400

    Update PyPI URL, drawdown tests

--- run_component_tests.sh
+++ run_component_tests.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 TEMP_DIR=$(mktemp -d)
-trap "rm -rf $TEMP_DIR" EXIT
+trap 'rm -rf $TEMP_DIR' EXIT
 
 echo "Creating test runner in $TEMP_DIR..."
 
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 cp -r "$SCRIPT_DIR/proof_of_portfolio/circuits/components" "$TEMP_DIR/components_standalone"
 
 echo "Running tests..."
-cd "$TEMP_DIR/components_standalone" && nargo test "$@"
+cd "$TEMP_DIR/components_standalone" && nargo test --show-output "$@"
